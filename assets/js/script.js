f08a64a87ea3de553f5db176b85564648d31ab58
Darren Behan <darrenbehan@hotmail.com>
2020-06-07 10:59:49 +1000

    changed the font awesome version to get floppy-o icon working, updated style.css to style the <span> added by script.js, added pseudocode to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,79 @@
+$(document).ready(function() {
+
 console.log("I'm loaded");
+
+// Create variables
+
+  // Create a variable to store the note entered
+
+// Create an array with each index holding a string of text from 9 AM to 5 PM
+
+// Create a function to load scheduler
+
+  // Create a for loop of an array 
+
+  // Create the html elements with classes
+
+    // Create a <div> with a class of "row"
+
+    // Create a <section> with classes of "input-group time-block"
+
+    // Create a <div> with a class of "input-group-prepend"
+
+    // Create a <span> with a class of "input-group-text" and a value from 9 to 5
+
+      // Assign text to the <span>
+
+    // Create a <textarea> with a class of "form-control" & aria-label="With textarea"
+    
+    // Create a <button> with classes of "saveBtn fa fa-floppy-o fa-2x btn btn-secondary"
+
+  // Append the elements as follows:
+
+    // (".row") appends to (".container")
+
+    // <section> appends to (".row")
+
+    // <div> (".input-group-prepend") appends to section
+
+    // span appends to div (".input-group-prepend")
+
+    // <textarea> appends to <section>
+
+    // <button> appends to <section>
+
+  // html with classes desired layout:
+
+  // <div class="row">
+  //   <section class="input-group time-block">
+  //     <div class="input-group-prepend">
+  //       <span class="input-group-text"></span>
+  //     </div>
+  //     <textarea class="form-control" aria-label="With textarea"></textarea>
+  //     <button class="saveBtn fa fa-floppy-o fa-2x btn btn-secondary"></button>
+  //   </section>
+  // </div>
+
+// As the clock moves into the next day
+
+  // Each <textarea> is reset to empty() value and assign the ".future" class
+
+// As the current hits, the first <textarea> is assigned the ".present" class
+
+// As the clock moves to the next hour, the relevant <textarea> is assigned the ".present" class with the previous hour ".past" class
+
+// Add Event Listeners
+
+  // Click event for button
+
+    // Stores the note to localStorage
+
+// Init
+
+  // localStorage getItem to append to the relevant ".row" <textarea>
+
+  // loads the function to display <textarea> colors based on the time
+
+  // loads moment.js
+
+});
